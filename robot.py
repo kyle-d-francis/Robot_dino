713e2fcff881ef42a2b16e31bf1142c0260b0179
kyle-d-francis <102608450+kyle-d-francis@users.noreply.github.com>
2022-04-04 11:34:40 -0500

    created robot class
    robot class has been partially created

---
+++ robot.py
@@ -0,0 +1,9 @@
+import weapon
+class robot:
+    def __init__(self) :
+        self.name = 'Gypsy Danger'
+        self.health = 100
+        self.active_weapon = weapon
+
+    def attack(self, dinosaur):
+        pass
--- weapon.py
+++ weapon.py
@@ -1,6 +1,6 @@
 class weapon:
     def __init__(self) :
-        self.weapon = ''
+        self.weapon = 'battle axe'
         self.attack_power = 20
 
 
