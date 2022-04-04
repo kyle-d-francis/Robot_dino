a964eddaa4b3cae41dd5a1725ff7d7f4ed6a28cd
kyle-d-francis <102608450+kyle-d-francis@users.noreply.github.com>
2022-04-04 13:13:42 -0500

    battlefield class created
    skeleton of the battlefield has been created

---
+++ battlfield.py
@@ -0,0 +1,18 @@
+import robot
+import dinosaur
+class battlefield:
+    def __init__(self):
+        self.robot = robot
+        self.dinosaur = dinosaur
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
--- dinosaur.py
+++ dinosaur.py
@@ -1,1 +1,8 @@
-class
+class dinosaur:
+    def __init__(self):
+        self.name = ''
+        self.attack_power = 15
+        self.health = 100
+    
+    def attack(self, robot):
+        pass
